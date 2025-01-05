a3e2a72c2da58b5bff190db981c4759b4886e8c9
Nikolai Zorin <nzorin@gmail.com>
2025-01-05 03:16:09 +0000

    favorites

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -25,17 +25,19 @@ class MovieDAO:
          # Define the Unit of Work
         def get_movies(tx, sort, order, limit, skip, user_id):
             # Define the cypher statement
+            favorites = self.get_user_favorites(tx, user_id)
+
             cypher = """
                 MATCH (m:Movie)
                 WHERE m.`{0}` IS NOT NULL
-                RETURN m {{ .* }} AS movie
+                RETURN m {{ .*, favorite: m.tmdbId in $favorites }} AS movie
                 ORDER BY m.`{0}` {1}
                 SKIP $skip
                 LIMIT $limit
             """.format(sort, order)
 
             # Run the statement within the transaction passed as the first argument
-            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id)
+            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id, favorites=favorites)
 
             # Extract a list of Movies from the Result
             return [row.value("movie") for row in result]
